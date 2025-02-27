c50fba3bcef021d926014dcd682d061920e44352
safrah710 <safeerahuman2004@gmail.com>
2025-02-28 00:48:12 +0530

    saved

---
+++ src/components/home/Project.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect, useCallback } from "react";
+import Container from "react-bootstrap/Container";
+import { Jumbotron } from "./migration";
+import Row from "react-bootstrap/Row";
+import ProjectCard from "./ProjectCard";
+import axios from "axios";
+
+
+const dummyProject = {
+  name: null,
+  description: null,
+  svn_url: null,
+  stargazers_count: null,
+  languages_url: null,
+  pushed_at: null,
+};
+
+const API = "https://api.github.com";
+// const gitHubQuery = "/repos?sort=updated&direction=desc";
+// const specficQuerry = "https://api.github.com/repos/hashirshoaeb/";
+
+const Project = ({ heading, username, length, specfic }) => {
+  const allReposAPI = `${API}/users/${username}/repos?sort=updated&direction=desc`;
+  const specficReposAPI = `${API}/repos/${username}`;
+  const dummyProjectsArr = new Array(length + specfic.length).fill(dummyProject);
+
+  const [projectsArray, setProjectsArray] = useState([]);
+  
+  // Replace with your GitHub token
+  const githubToken = process.env.REACT_APP_GITHUB_TOKEN; 
+
+  const fetchRepos = useCallback(async () => {
+    let repoList = [];
+    try {
+      // Adding Authorization header with the GitHub token
+      const headers = {
+        Authorization: `Bearer ${githubToken}`,
+      };
+
+      // Getting all repos
+      const response = await axios.get(allReposAPI, { headers });
+      // Slicing to the length
+      repoList = [...response.data.slice(0, length)];
+
+      // Adding specified repos
+      try {
+        for (let repoName of specfic) {
+          const response = await axios.get(`${specficReposAPI}/${repoName}`, {
+            headers,
+          });
+          repoList.push(response.data);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+
+      // Setting projectArray
+      setProjectsArray(repoList);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [allReposAPI, length, specfic, specficReposAPI, githubToken]);
+
+  useEffect(() => {
+    fetchRepos();
+  }, [fetchRepos]);
+
+  return (
+    <Jumbotron fluid id="projects" className="bg-light m-0">
+      <Container>
+        <h2 className="display-4 pb-5 text-center">{heading}</h2>
+        <Row>
+          {projectsArray.length
+            ? projectsArray.map((project, index) => (
+                <ProjectCard
+                  key={`project-card-${index}`}
+                  id={`project-card-${index}`}
+                  value={project}
+                />
+              ))
+            : dummyProjectsArr.map((project, index) => (
+                <ProjectCard
+                  key={`dummy-${index}`}
+                  id={`dummy-${index}`}
+                  value={project}
+                />
+              ))}
+        </Row>
+      </Container>
+    </Jumbotron>
+  );
+};
+
+export default Project;
---
+++ src/editable-stuff/config.js
@@ -0,0 +1,144 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  firstName: "Safeekur",
+  middleName: "",
+  lastName: "Rahman",
+  message: " Passionate about changing the world with technology. ",
+  icons: [
+    {
+      image: "fa-github",
+      url: "https://github.com/safrah710",
+    },
+    {
+      image: "fa-instagram",
+      url: "https://www.instagram.com/hashirshoaeb/",
+    },
+    {
+      image: "fa-linkedin",
+      url: "http://www.linkedin.com/in/safeekur-rahman-ibnu-zakriya-024489252",
+    },
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+
+const about = {
+  show: true,
+  heading: "About Me",
+  imageLink: require("../editable-stuff/safeekur.png"),
+  imageSize: 400,
+  message:
+    "Hi, I'm Safeekur Rahman, an enthusiastic computer science student with a strong foundation in coding and software development. I have hands-on experience in building web applications, solving complex problems, and working with AI-based solutions. My passion lies in applying my technical skills to real-world projects, continuously learning, and contributing to innovative solutions.",
+  resume: "https://drive.google.com/file/d/1auTyqDbG4sPIPb0ZvkA-YAjoJiRUELVm/view?usp=drivesdk",
+};
+
+// PROJECTS SECTION
+// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+const repos = {
+  show: true,
+  heading: "Recent Projects",
+  gitHubUsername: "safrah710", //i.e."johnDoe12Gh"
+  reposLength: 4,
+  specificRepos: [],
+};
+
+// Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    { 
+      img: require("../editable-stuff/safeekur.png"), 
+      label: "First slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+    { 
+      img: require("../editable-stuff/safeekur.png"), 
+      label: "Second slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+  ],
+  imageSize: {
+    width:"615",
+    height:"450"
+  }
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  heading: "Skills",
+  hardSkills: [
+    { name: "React", value: 55 },
+    { name: "Python", value: 40 },
+    { name: "SQL", value: 65 },
+    { name: "Data Structures", value: 75 },
+    { name: "C/C++", value: 65 },
+    { name: "JavaScript", value: 90 },
+    { name: "HTML/CSS", value: 65 },
+  ],
+  softSkills: [
+    { name: "Goal-Oriented", value: 80 },
+    { name: "Collaboration", value: 90 },
+    { name: "Adaptability", value: 85 },
+    { name: "Problem Solving", value: 75 },
+    { name: "Organization", value: 70 },
+    { name: "Creativity", value: 90 },
+  ],
+};
+
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "Get In Touch",
+  message:
+    "I’m on the lookout for full-time Software Engineering or Machine Learning opportunities! If you know of any cool openings, have questions, or just want to chat about the latest tech trends (or pizza toppings), feel free to drop me an email at",
+  email: "safeerahuman2004@gmail.com",
+};
+
+const experiences = {
+  show: false,
+  heading: "Experiences",
+  data: [
+    {
+      role: 'Software Engineer',// Here Add Company Name
+      companylogo: require('../assets/img/dell.png'),
+      date: 'June 2018 – Present',
+    },
+    {
+      role: 'Front-End Developer',
+      companylogo: require('../assets/img/boeing.png'),
+      date: 'May 2017 – May 2018',
+    },
+  ]
+}
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
